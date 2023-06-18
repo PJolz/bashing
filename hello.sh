10964bfca8627dffd1fe98ea9ee351589fda8bad
PJolz <PJolz@users.noreply.github.com>
2023-06-18 12:58:31 +0200

    Adding the hello.sh file to the repo.

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Comments are not executed
+
+echo "Hello, User!"  # Also this bit not executed
+echo "Please enter your full name: "
+read -a userName
+echo "Hello, $userName! Your first name is ${userName[0]}, and your last name is ${userName[1]}."
+
