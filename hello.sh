86c1961674f78f43b2ccdaf2749f4bbe1e2b11b0
PJolz <PJolz@users.noreply.github.com>
2023-06-18 17:03:43 +0200

    Committing the changes

--- hello.sh
+++ hello.sh
@@ -6,4 +6,5 @@ echo "Hello, User!"  # Also this bit not executed
 echo "Please enter your full name: "
 read -a userName
 echo "Hello, $userName! Your first name is ${userName[0]}, and your last name is ${userName[1]}."
+echo "Making this update for testing purposes"  #Just a test comment
 
